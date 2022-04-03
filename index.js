e23bd3ceaf0db86adce827f366b7a9b306b33dcd
Terence Holmes <piece4ulmind@gmail.com>
2022-04-03 14:55:18 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record) {
+
+   const records = record.find(({result}) => result === "W")
+
+   if (records) {
+      return records.year
+      } 
+}
+superbowlWin(record);
+superbowlWin(sadReality);
